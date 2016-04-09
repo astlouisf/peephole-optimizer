47758ed4a7eb1d04fcff961369ab132be3a8e998
Stefan Knudsen <stefan.knudsen@mail.mcgill.ca>
2016-04-09 15:30:30 -0400

    added compile for benchmark 3

--- PeepholeBenchmarks/compileall.sh
+++ PeepholeBenchmarks/compileall.sh
@@ -1,12 +1,16 @@
-# doesn't include benchmark 3 or 6
-# bench01 bench02 bench03 bench04 bench05 bench06 bench07
-for benchmark in bench01 bench02 bench04 bench05 bench07
+for benchmark in bench01 bench02 bench04 bench05 bench07 # doesn't include benchmark 6
 do
 cd $benchmark
 # $PEEPDIR/joosc *.java	# unoptimized
 $PEEPDIR/joosc -O *.java
 cd ..
 done
+
+# benchmark 3
+cd $benchmark
+$PEEPDIR/joosc -O *.java *.joos
+cd ..
+
 
 # awk '{sum += $3} END {print sum}' bench0*/dump.size	# unoptimized
 echo ""
